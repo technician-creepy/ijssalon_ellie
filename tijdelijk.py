353347e4c7af2faa425f6215fd9a4ee13fca4428
Patrick-Kox <technician-creepy@proton.me>
2024-07-12 13:09:41 +0200

    Aangepast naar begin Les 8

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,42 +1,21 @@
-# Les 7 Huiswerk 1
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade": 5
+}
 
-#2
-prijzen = {"aardbei" : "3",
-           "vanille" : "4",
-           "chocolade": "5"}
+aanbieding = prijzen["aardbei"] * 0.8
 
-#3
-aanbieding = float(prijzen["aardbei"]) * 0.8
-
-#4
 reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}")
-# print(reclame_tekst)
 
-#5
-decimaal = reclame_tekst.index("0")
-reclame_tekst2 = reclame_tekst[:decimaal]
-# print (reclame_tekst2)
+reclame_tekst2 = reclame_tekst[:63]
 
-#6
 reclame_tekst3 = reclame_tekst2.upper()
-# print (reclame_tekst3)
 
-#7
-reclame_tekst4 = reclame_tekst3.split(" ")
-# print (reclame_tekst4)
+reclame_tekst4 = reclame_tekst3.split()
 
-#8
 for el in reclame_tekst4:
-    print (el)
-
-#9
-for el in reclame_tekst4:
-    print (el.lower())
-
-#10
-for el in reclame_tekst4:
-    count = len(el)
-    if (int(count) >= 5):
+    if len(el) >4:
         print(el.upper())
     else:
         print(el.lower())
