86e7b466aa0c6bbcd10ffeae972ece1ee0a3f67a
Patrick-Kox <technician-creepy@proton.me>
2024-07-12 15:00:18 +0200

    tussentijde update met testcode

--- reclame.py
+++ reclame.py
@@ -1,15 +1,47 @@
+from algemene_functies import mijn_functie2
+
 def aanbieding_1(smaak, prijs, korting):
     tekst_1 = "Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak "
     tekst_2 = (f"{smaak}, van {prijs} euro voor {prijs - prijs * korting}")
 
     return tekst_1 + tekst_2
 
-def inkomsten_totaal(inkomsten):
+def inkomsten_totaal(inkomsten, btw):
+    totaal = 0
     for bedrag in inkomsten:
-        bedrag = bedrag + bedrag
-    return bedrag
+        totaal = totaal + bedrag
+    
+    btw = totaal * btw
+    totale_inkomsten = (f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw} euro btw betaald dient te worden.")
+    return totale_inkomsten
 
-# print(aanbieding_1("Aardbei", 4, 0.1))
+def laag_en_hoog(mijn_lijst):
+    hoogste = max(mijn_lijst)
+    laagste = min(mijn_lijst)
+    return [hoogste, laagste]
 
-# inkomsten = [220, 430, 125, 160, 205, 90, 345]
+def gemiddelde(mijn_lijst):
+    totaal = 0
+    for inkomst in mijn_lijst:
+        totaal = totaal + inkomst
+    week_gemiddelde = totaal / 7
+    gemiddeld = (f"De gemiddelde inkomsten deze week zijn {week_gemiddelde} euro")
+    return gemiddeld
+
+def meervoudig(invoer_lijst):
+    return laag_en_hoog(invoer_lijst)
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie2(korte_lijst[0],korte_lijst[1])
+    return uitvoer
+
+print(aanbieding_1("Aardbei", 4, 0.1))
+inkomsten = [220, 430, 125, 160, 205, 90, 345]
+print(inkomsten_totaal(inkomsten,0.09))
+print(laag_en_hoog(inkomsten))
+invoer_lijst = [10,5,3,2,1,2,9]
+print(meervoudig(invoer_lijst))
+print(combinatie(inkomsten))
+
 
