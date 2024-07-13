13ce1d239553e0b17d682466d9bd8410c569a43b
Patrick-Kox <technician-creepy@proton.me>
2024-07-13 18:35:30 +0200

    Aangepast voor huiswerk

--- helper.py
+++ helper.py
@@ -6,3 +6,14 @@ def decoreer(tekst=""):
     print(lengte * "*")
     print()
 
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag / personen
+    except:
+        bedrag_pp = "??"
+    return f"HetBedrag per persoon is {bedrag_pp} euro."
+
+b = int(input("Welk bedrag zit er in de fooienpot? "))
+p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+
+print(fooi_pp(b,p))
