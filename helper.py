e910d3000021d97b4792e3e179e3bb89c8d30910
Patrick-Kox <technician-creepy@proton.me>
2024-07-13 19:05:20 +0200

    Les 10 afgewerkt

--- helper.py
+++ helper.py
@@ -10,3 +10,12 @@ def decoreer(tekst=""):
 def fooi_pp(bedrag, personen):
     bedrag_pp = bedrag/personen
     return f"Het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+print(onderstreep("Test"))
+
